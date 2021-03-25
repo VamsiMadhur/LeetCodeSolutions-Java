8f67671206cca84307c361567f3ec96cfab37bff
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-03-25 08:56:23 +0000

    Sync LeetCode submission - Unique Binary Search Trees (java)

---
+++ LeetCodeSolutions/problems/unique_binary_search_trees/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    
+    public int numTrees(int n) {
+        int[] result = new int[n+1];
+        result[0] = 1;
+        result[1] = 1;
+        for(int i=2; i<=n; i++) {
+            int sum = 0;
+            for(int j=1; j<=i; j++) {
+                sum += result[j-1]*result[i-j];
+            }
+            result[i] = sum;
+        }
+        
+        return result[n];
+    }
+}
