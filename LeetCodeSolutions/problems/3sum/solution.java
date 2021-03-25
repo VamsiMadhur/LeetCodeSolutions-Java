ae8c5633a05ed91115188dec5d585bf4fd3716f6
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-03-25 11:39:40 +0000

    Sync LeetCode submission - 3Sum (java)

--- LeetCodeSolutions/problems/3sum/solution.java
+++ LeetCodeSolutions/problems/3sum/solution.java
@@ -1,21 +1,24 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-       Set<List<Integer>> res = new HashSet<>();
-        Set<Integer> dups = new HashSet<>();
-        Map<Integer, Integer> seen = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i) {
-            if (dups.add(nums[i])) {
-                for (int j = i + 1; j < nums.length; ++j) {
-                    int complement = -nums[i] - nums[j];
-                    if (seen.containsKey(complement) && seen.get(complement) == i) {
-                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
-                        Collections.sort(triplet);
-                        res.add(triplet);
-                    }
-                    seen.put(nums[j], i);
+        Arrays.sort(nums);
+        Set<List<Integer>> result = new HashSet<>();
+        
+        for(int left=0; left<nums.length-2; left++) {
+            int mid = left+1;
+            int right = nums.length-1;
+            while(mid<right) {
+                int sum = nums[left] + nums[mid] + nums[right];
+                if(sum<0) {
+                    mid++;
+                } else if(sum > 0) {
+                    right--;
+                } else {
+                    List<Integer> triplet = Arrays.asList(nums[left], nums[mid], nums[right]);
+                    result.add(triplet);
+                    mid++;
                 }
             }
         }
-        return new ArrayList(res);
-    }
+        return new ArrayList<>(result);
+    }    
 }
