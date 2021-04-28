65eff2a90b3e8d48ba57641306243563f83799bb
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-04-28 17:49:19 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock II (java)

--- LeetCodeSolutions/problems/best_time_to_buy_and_sell_stock_ii/solution.java
+++ LeetCodeSolutions/problems/best_time_to_buy_and_sell_stock_ii/solution.java
@@ -1,23 +1,10 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit = 0;
-        int buyPrice = prices[0];
-        int sellPrice = 0;
+        int maxProfit = 0;
         for(int i=1; i<prices.length; i++) {
-            if(sellPrice==0 && buyPrice > prices[i]) {
-                buyPrice = prices[i];
-            } else if(sellPrice < prices[i]) {
-                sellPrice = prices[i];
-            } else {
-                profit += (sellPrice - buyPrice);
-                sellPrice = 0;
-                buyPrice = prices[i];
-            }
+            if(prices[i]>prices[i-1])
+                maxProfit += (prices[i]-prices[i-1]);
         }
-        if(sellPrice != 0) {
-            profit += (sellPrice - buyPrice);
-        }
-        return profit;
-        
+        return maxProfit;
     }
 }
