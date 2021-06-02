064ee127059261645c96973696668041bb432b0f
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-06-02 17:52:58 +0000

    Sync LeetCode submission - Set Matrix Zeroes (java)

--- LeetCodeSolutions/problems/set_matrix_zeroes/solution.java
+++ LeetCodeSolutions/problems/set_matrix_zeroes/solution.java
@@ -1,34 +1,53 @@
 class Solution {
-    public void setZeroes(int[][] matrix) {
-        int noOfRows = matrix.length;
-        int noOfColumns = matrix[0].length;
-    
-        int [] rows = new int[noOfRows];
-        int [] columns = new int[noOfColumns];
+    public void setZeroes(int[][] input) {
+    	int m = input.length;
+		int n = input[0].length;
+		
+		boolean isFirstRow = false, isFirstColumn = false;
+		
+		for(int i=0; i<m; i++) {
+			if(input[i][0] == 0) {
+				isFirstColumn = true;
+				break;
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			if(input[0][i] == 0) {
+				isFirstRow = true;
+				break;
+			}
+		}
+		
+		for(int i=1; i<m; i++) {
+			for(int j=1; j<n; j++) {
+				if(input[i][j] == 0) {
+					input[i][0] = 0;
+					input[0][j] = 0;
+				}
+			}
+		}
+		
+		for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+			    if(input[i][0] == 0 || input[0][j]==0) {
+					input[i][j] = 0;
+				}
+			}
+		}
         
-        for(int i=0; i<noOfRows; i++) {
-            for(int j=0; j<noOfColumns; j++) {
-                if(matrix[i][j]==0) {
-                    rows[i] = 1;
-                    columns[j] = 1;
-                }
-            }
-        }
+		if(isFirstRow) {
+			for(int j=0; j<n; j++) {
+				input[0][j] = 0;
+			}
+		}
+		
+		if(isFirstColumn) {
+			for(int i=0; i<m; i++) {
+				input[i][0] = 0;
+			}
+		}
+		
         
-        for(int k=0; k<noOfRows; k++) {
-            if(rows[k]==1) {
-                for(int i=0; i<noOfColumns; i++) {
-                    matrix[k][i]=0;
-                }
-            }
-        }
-
-        for(int k=0; k<noOfColumns; k++) {
-            if(columns[k]==1) {
-                for(int i=0; i<matrix.length; i++) {
-                    matrix[i][k]=0;
-                }
-            }
-        }
     }
 }
