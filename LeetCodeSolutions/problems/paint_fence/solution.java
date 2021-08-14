297d1017cd4edb367341c2cb26a6319da5ba9cda
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-08-14 00:54:09 +0000

    Sync LeetCode submission - Paint Fence (java)

--- LeetCodeSolutions/problems/paint_fence/solution.java
+++ LeetCodeSolutions/problems/paint_fence/solution.java
@@ -1,10 +1,14 @@
 class Solution {
     public int numWays(int n, int k) {
         if(n==1) return k;
+        if(n==2) return k*k;
+        
         int lastBut1 = k;
         int last = k*k;
+        
         for(int i=3; i<=n; i++) {
-            int temp = (last + lastBut1)*(k-1);
+            
+            int temp = (k-1)*(lastBut1 + last);
             lastBut1 = last;
             last = temp;
         }
