99f3f08a0ebc91433304b03d617e6f504cbd0305
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-08-22 16:45:15 +0000

    Sync LeetCode submission - Roman to Integer (java)

--- LeetCodeSolutions/problems/roman_to_integer/solution.java
+++ LeetCodeSolutions/problems/roman_to_integer/solution.java
@@ -1,27 +1,34 @@
 class Solution {
+    
+    private static Map<Character, Integer> mapper = new HashMap<>();
+    
+    static {
+        mapper.put('I', 1);
+        mapper.put('V', 5);
+        mapper.put('X', 10);
+        mapper.put('L', 50);
+        mapper.put('C', 100);
+        mapper.put('D', 500);
+        mapper.put('M', 1000);
+    }
+    
     public int romanToInt(String s) {
-        
-        Map<Character, Integer> converter = new HashMap<Character, Integer>(7);
-        converter.put('I',1);
-        converter.put('V',5);
-        converter.put('X',10);
-        converter.put('L',50);
-        converter.put('C',100);
-        converter.put('D',500);
-        converter.put('M',1000);
-        
-        int totalValue = 0;
-        int previousValue = 0;
+        int answer = 0;
+        int prev = 0;
         for(int i=0; i<s.length(); i++) {
-            int currentValue = converter.get(s.charAt(i));
-            if(previousValue == 0 || previousValue >= currentValue) {
-                totalValue += currentValue;
+            int currValue = mapper.get(s.charAt(i));
+            int nextValue = 0;
+            if(i+1 < s.length()) {
+                nextValue = mapper.get(s.charAt(i+1));
+            }
+            if(nextValue > currValue) {
+                answer += (nextValue - currValue);
+                i++;
             } else {
-                totalValue = totalValue - (2*previousValue) + currentValue;
-            }                       
-            previousValue = currentValue;
+                answer += currValue;
+            }
         }
+        return answer;
         
-        return totalValue;
     }
 }
