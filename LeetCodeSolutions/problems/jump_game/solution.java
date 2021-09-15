801932c290764f782eb8dd22be93ae4e6e09ae87
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-09-15 18:07:22 +0000

    Sync LeetCode submission - Jump Game (java)

--- LeetCodeSolutions/problems/jump_game/solution.java
+++ LeetCodeSolutions/problems/jump_game/solution.java
@@ -2,19 +2,19 @@ class Solution {
     public boolean canJump(int[] nums) {
         boolean flag = true;
         for(int i = nums.length-2; i>=0; i--) {
-            if(nums[i]==0) {
-                flag = false;
-                int count=2;
-                i--;
-                while(i>=0) {
-                    if(nums[i] >= count) {
-                        flag = true;
-                        break;
-                    }
-                    count++;
-                    i--;
-                }
-            }
+          if(nums[i]==0) {
+              flag = false;
+              int count = 2;
+              i--;
+              while(i>=0) {
+                  if(nums[i]>=count) {
+                      flag = true;
+                      break;
+                  }
+                 i--;
+                count++;
+              }
+          }
         }
         return flag;
     }
