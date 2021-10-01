ffb8849f0e8a03dd083125e5e721b0cc94cf194e
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-10-01 04:12:21 +0000

    Sync LeetCode submission - Pascal's Triangle II (java)

--- LeetCodeSolutions/problems/pascal's_triangle_ii/solution.java
+++ LeetCodeSolutions/problems/pascal's_triangle_ii/solution.java
@@ -1,43 +1,18 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
+        
         List<Integer> result = new ArrayList<Integer>();
         result.add(1);
-        if(rowIndex == 0) {
-            return result;
+        
+        int numerator = rowIndex;
+        int denominator = 1;
+        int prev = 1;
+        for(int i=0; i<rowIndex; i++) {
+            prev = (int)(((long)prev*numerator)/denominator);
+            numerator--;
+            denominator++;
+            result.add(prev);
         }
-        Map<Integer, Integer> ncrSet = new HashMap<Integer, Integer>();
-        for(int i=1; i<rowIndex; i++) {
-            int val = 0;
-            if(ncrSet.containsKey(rowIndex-i)) {
-                val = ncrSet.get(rowIndex-i);
-            }else {
-                val = nCr(rowIndex,i);    
-                ncrSet.put(i, val);
-            }
-            result.add(val);
-        }
-        result.add(1);
         return result;
-    }
-    
-    private int nCr(int n, int r) {
-        long result = (n-r+1);
-        int i=(n-r+1);
-        int j=2;
-        int k=2;
-        while(j<=r) {
-            i++;
-            result *= i;
-            j++;
-            while(k<=r && result%k == 0) {
-                result /= k;
-                k++;
-            }
-        }
-        while(k<=r && result%k == 0) {
-            result /= k;
-            k++;
-        }
-        return (int)result;
     }
 }
