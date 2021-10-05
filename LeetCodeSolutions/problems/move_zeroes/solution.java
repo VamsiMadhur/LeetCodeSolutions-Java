c29da52694fe30b957457d96be352c0fe91de86a
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-10-05 04:17:07 +0000

    Sync LeetCode submission - Move Zeroes (java)

--- LeetCodeSolutions/problems/move_zeroes/solution.java
+++ LeetCodeSolutions/problems/move_zeroes/solution.java
@@ -1,17 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int p = -1;
-        int q = -1;
-        for(int num : nums) {
-            if(num!=0) {
-                if(q<p) {
-                    int temp = nums[p+1];
-                    nums[p+1] = nums[q+1];
-                    nums[q+1] = temp;
-                }
-                q++;
+        int k = 0;
+        int n = nums.length;
+        for(int i=0; i<n; i++) {
+            if(nums[i] != 0) {
+                nums[k] = nums[i];
+                k++;
             }
-            p++;
+        }
+        for(int i=k; i<n; i++) {
+            nums[i] = 0;
         }
     }
 }
