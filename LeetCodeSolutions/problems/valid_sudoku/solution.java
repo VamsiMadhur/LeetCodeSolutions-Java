ec6409d46fabc4ff5434492e383114f663541d09
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-10-09 19:00:19 +0000

    Sync LeetCode submission - Valid Sudoku (java)

--- LeetCodeSolutions/problems/valid_sudoku/solution.java
+++ LeetCodeSolutions/problems/valid_sudoku/solution.java
@@ -3,16 +3,14 @@ class Solution {
         return isHValid(board) && isVValid(board) && isSValid(board);
     }
     
-    public int check(char[][] board, int i, int j, int seen) {
-        
+    public int check(char[][] board, int i, int j,int seen) {
         if(board[i][j] == '.') return seen;
         
         int num = board[i][j] - '0';
-        
         int curr = 1 << (num-1);
         if((seen & curr) >= 1) return -seen;
         
-        return seen|curr;
+        return (seen | curr);
     }
     
     public boolean isHValid(char[][] board) {
@@ -20,9 +18,7 @@ class Solution {
             int seen = 0;
             for(int j=0; j<9; j++) {
                 seen = check(board, i, j, seen);
-                if(seen < 0) {
-                    return false;
-                }
+                if(seen < 0) return false;
             }
         }
         return true;
@@ -33,50 +29,46 @@ class Solution {
             int seen = 0;
             for(int i=0; i<9; i++) {
                 seen = check(board, i, j, seen);
-                if(seen < 0) {
-                    return false;
-                }
-
+                if(seen < 0) return false;
             }
         }
         return true;
     }
     
     public boolean isSValid(char[][] board) {
-        for(int i=0; i<9; i=i+3) {
-            for(int j=0; j<9; j=j+3) {
+        for(int i=0; i<9; i+=3) {
+            for(int j=0; j<9; j+=3) {
                 int seen = 0;
                 
                 seen = check(board, i, j, seen);
                 if(seen < 0) return false;
                 
+                seen = check(board, i, j+1, seen);
+                if(seen < 0) return false;
+                
+                seen = check(board, i, j+2, seen);
+                if(seen < 0) return false;
+                
                 seen = check(board, i+1, j, seen);
+                if(seen < 0) return false;
+                
+                seen = check(board, i+1, j+1, seen);
+                if(seen < 0) return false;
+                
+                seen = check(board, i+1, j+2, seen);
                 if(seen < 0) return false;
                 
                 seen = check(board, i+2, j, seen);
                 if(seen < 0) return false;
-                    
-                seen = check(board, i, j+1, seen);
-                if(seen < 0) return false;
-
-                seen = check(board, i+1, j+1, seen);
-                if(seen < 0) return false;
-
+                
                 seen = check(board, i+2, j+1, seen);
                 if(seen < 0) return false;
-
-                seen = check(board, i, j+2, seen);
-                if(seen < 0) return false;
-
-
-                seen = check(board, i+1, j+2, seen);
-                if(seen < 0) return false;
-
-
+                
                 seen = check(board, i+2, j+2, seen);
                 if(seen < 0) return false;
             }
         }
         return true;
     }
+      
 }
