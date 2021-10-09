51fd7ad06a229ae2894469a3be026b1c3998097c
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-10-09 19:09:14 +0000

    Sync LeetCode submission - Search a 2D Matrix (java)

--- LeetCodeSolutions/problems/search_a_2d_matrix/solution.java
+++ LeetCodeSolutions/problems/search_a_2d_matrix/solution.java
@@ -1,28 +1,22 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int rows = matrix.length;
-        if(rows == 0) {
-            return false;
-        }
-        int columns = matrix[0].length;
-        if(columns == 0) {
-            return false;
-        }
+        int m = matrix.length;
+        int n = matrix[0].length;
         
-        int start = 0, end = (rows*columns)-1;
-        while(start<=end) {
-            int mid = (start + end)/2;
-            int row = mid/columns;
-            int column = mid%columns;
-            if(target == matrix[row][column]) {
+        int i = 0;
+        int j =(m*n)-1;
+        
+        while(i <= j) {
+            int mid = i+ ((j-i)/2);
+            int r = mid/n, c = mid%n;
+            if(matrix[r][c] == target) {
                 return true;
-            } else if(target > matrix[row][column]) {
-                start = mid+1;
+            } else if(matrix[r][c] < target) {
+                i = mid+1;
             } else {
-                end = mid-1;
+                j = mid-1;
             }
         }
         return false;
-        
     }
 }
