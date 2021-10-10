91b8131bb84a96d79d76c84a89cabdd3b48bd074
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-10-10 20:32:39 +0000

    Sync LeetCode submission - Bitwise AND of Numbers Range (java)

--- LeetCodeSolutions/problems/bitwise_and_of_numbers_range/solution.java
+++ LeetCodeSolutions/problems/bitwise_and_of_numbers_range/solution.java
@@ -1,9 +1,8 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
-        int shift = 0;
-        while(left<right) {
+        while(left < right) {
             right = right & (right-1);
         }
-        return left&right;
+        return left & right;
     }
 }
