27c9bef5e916c44ebd542a3c2d2b1b3f91970b54
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-10-11 03:13:53 +0000

    Sync LeetCode submission - Valid Parentheses (java)

--- LeetCodeSolutions/problems/valid_parentheses/solution.java
+++ LeetCodeSolutions/problems/valid_parentheses/solution.java
@@ -1,27 +1,25 @@
-
 class Solution {
     public boolean isValid(String s) {
-        Map<Character, Character> mapper = new HashMap<Character, Character>(3);
-        mapper.put('(', ')');
-        mapper.put('[', ']');
-        mapper.put('{', '}');
+        char[] stack = new char[s.length()];
+        char[] sa = s.toCharArray();
+        int count = 0;
         
-        
-        Stack<Character> stack = new Stack<Character>();
-        for(int i=0; i<s.length(); i++) {
-            Character curr = s.charAt(i);
-            if(mapper.containsKey(curr)) {
-                stack.push(curr);
+        for(int i=0; i<sa.length; i++) {
+            if(sa[i] == '(' || sa[i] == '{' || sa[i] == '[') {
+                stack[count++] = sa[i];
             } else {
-                if(stack.empty()) {
+                if(count == 0) return false;
+                
+                char temp = stack[--count];
+                if((sa[i] == ')' && temp == '(')
+                        || (sa[i] == '}' && temp == '{')
+                        || (sa[i] == ']' && temp == '[')) {
+                    continue;
+                } else {
                     return false;
-                }
-                Character sr = stack.pop();
-                if(mapper.get(sr) != curr) {
-                     return false;    
                 }
             }
         }
-        return stack.empty();
+        return count==0;
     }
 }
