65b9be3df2fbdc52c71dcf550fa215b7246a3895
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-10-16 16:29:40 +0000

    Sync LeetCode submission - Two Sum IV - Input is a BST (java)

--- LeetCodeSolutions/problems/two_sum_iv_-_input_is_a_bst/solution.java
+++ LeetCodeSolutions/problems/two_sum_iv_-_input_is_a_bst/solution.java
@@ -15,21 +15,18 @@
  */
 class Solution {
     public boolean findTarget(TreeNode root, int k) {
-        if(root == null || (root.left==null && root.right==null)){
-            return false;
-        }
+        if(root == null) return false;
         List<Integer> list = new ArrayList<>();
         inorder(root, list);
-        int l = 0;
-        int r = list.size()-1;
-        while(l<r) {
-            int sum = list.get(l) + list.get(r);
+        int i = 0;
+        int j = list.size()-1;
+        while(i<j) {
+            int sum = list.get(i) + list.get(j);
             if(sum == k) return true;
-            else if(sum < k) l++;
-            else r--;
+            else if(sum < k) i++;
+            else j--;
         }
         return false;
-        
     }
     
     public void inorder(TreeNode root, List<Integer> list) {
