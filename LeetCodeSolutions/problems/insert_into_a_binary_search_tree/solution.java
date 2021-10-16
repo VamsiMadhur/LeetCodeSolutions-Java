d9808a3de744a24260112a0c54dedd8fab49e925
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-10-16 15:43:23 +0000

    Sync LeetCode submission - Insert into a Binary Search Tree (java)

---
+++ LeetCodeSolutions/problems/insert_into_a_binary_search_tree/solution.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null) return new TreeNode(val);
+        TreeNode temp = root;
+        TreeNode prev = null;
+        while(temp != null) {
+            prev = temp;
+            if(temp.val > val)  temp = temp.left;
+            else temp = temp.right;
+        }
+        if(prev.val > val) prev.left = new TreeNode(val);
+        else prev.right = new TreeNode(val);
+        return root;
+    }
+}
