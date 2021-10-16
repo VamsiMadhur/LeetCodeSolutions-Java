50727343110a3bed2386acd68bcd918a0245f34a
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-10-16 18:54:35 +0000

    Sync LeetCode submission - Coin Change (java)

--- LeetCodeSolutions/problems/coin_change/solution.java
+++ LeetCodeSolutions/problems/coin_change/solution.java
@@ -3,14 +3,14 @@ class Solution {
         int max = amount+1;
         int[] dp = new int[max];
         Arrays.fill(dp, max);
-        dp[0] = 0;
-        for(int i=1; i<max; i++) {
+        dp[0]=0;
+        for(int i=1; i<max; i++ ) {
             for(int j=0; j<coins.length; j++) {
                 if(coins[j] <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                    dp[i] = Math.min(dp[i], 1 + dp[i-coins[j]]);
                 }
             }
         }
-        return dp[amount] > amount ? -1 : dp[amount];
+        return dp[amount] == max ? -1 : dp[amount];
     }
 }
