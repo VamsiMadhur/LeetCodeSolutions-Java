dfe2d67d72f1eb35434239d958a361fc75dfbeb4
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-11-08 22:08:39 +0000

    Sync LeetCode submission - Course Schedule II (java)

--- LeetCodeSolutions/problems/course_schedule_ii/solution.java
+++ LeetCodeSolutions/problems/course_schedule_ii/solution.java
@@ -1,50 +1,54 @@
-class GNode {
-    public int inDegree = 0;
-    public List<Integer> outNodes = new ArrayList<>();
+class Node {
+    int inDegree = 0;
+    List<Integer> outEdges = new ArrayList<>();
 }
 
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        Map<Integer, GNode> graph = new HashMap<>();
-        for(int[] relation : prerequisites) {
-            GNode prevNode = graph.computeIfAbsent(relation[1], k -> new GNode());
-            GNode nextNode = graph.computeIfAbsent(relation[0], k -> new GNode());
-            
-            prevNode.outNodes.add(relation[0]);
-            nextNode.inDegree += 1;
+        
+        Map<Integer, Node> graph = new HashMap<>();
+        for(int i=0; i<numCourses; i++) {
+            graph.put(i, new Node());
         }
         
-        int[] rootCourses = new int[numCourses];
-        int size = 0;
-        for(int j=0; j<numCourses; j++) {
-            GNode node = graph.computeIfAbsent(j, k -> new GNode());
+        for(int[] prereq : prerequisites) {
+            Node first = graph.get(prereq[0]);
+            Node second = graph.get(prereq[1]);
+            
+            first.inDegree++;
+            second.outEdges.add(prereq[0]);
+        }
+        
+        int[] result = new int[numCourses];
+        int index = 0;
+        for(int i=0; i<numCourses; i++) {
+            Node node = graph.get(i);
             if(node.inDegree == 0) {
-                rootCourses[size++] = j;
-            }
+                result[index++] = i;
+            }        
         }
         
         int i=0;
         int removedEdges = 0;
         int totalEdges = prerequisites.length;
-        while(i<size) {
-            Integer course = rootCourses[i];
+        while(i < index) {
+            int course = result[i];
+            Node n = graph.get(course);
             
-            for(Integer nextCourse : graph.get(course).outNodes) {
-                
-                GNode childNode = graph.get(nextCourse);
-                childNode.inDegree -= 1;
-                removedEdges += 1;
+            for(int child : n.outEdges) {
+                Node childNode = graph.get(child);
+                childNode.inDegree--;
+                removedEdges++;
                 if(childNode.inDegree == 0) {
-                    rootCourses[size++] = nextCourse;
+                    result[index++] = child;
                 }
             }
             i++;
         }
-        
-        if(totalEdges != removedEdges) {
-            return new int[0];
+        if(totalEdges == removedEdges) {
+            return result;
         } else {
-            return rootCourses;
+            return new int[0];
         }
         
     }
