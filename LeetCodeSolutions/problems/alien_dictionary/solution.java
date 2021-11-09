fa7b90025e3d24ae45566da344c917d7e520db88
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-11-09 19:28:18 +0000

    Sync LeetCode submission - Alien Dictionary (java)

--- LeetCodeSolutions/problems/alien_dictionary/solution.java
+++ LeetCodeSolutions/problems/alien_dictionary/solution.java
@@ -1,65 +1,66 @@
 class GNode {
-    public int inDegree = 0;
-    public List<Character> outNodes = new ArrayList<>();
+    int inDegree = 0;
+    List<Character> outedges = new ArrayList<>();
 }
 
 class Solution {
-    
     public String alienOrder(String[] words) {
-        Map<Character, GNode> graph = new HashMap<>();
         
-        //Get All GNodes for all letters
+        int n = words.length;
+        
+        Map<Character, GNode> graph = new HashMap<>();
         for(String word : words) {
             for(char c : word.toCharArray()) {
-                graph.computeIfAbsent(c, k-> new GNode());
-            }
+                graph.computeIfAbsent(c, k -> new GNode());
+            } 
         }
-        //buildGraph(graph, words);
-        for(int i=0; i<words.length-1; i++) {
+        
+        for(int i=0; i<n-1; i++) {
             String word1 = words[i];
             String word2 = words[i+1];
             
-            if(word1.length() > word2.length() && word1.startsWith(word2)) return "";
+            if(word1.length() > word2.length() && word1.startsWith(word2)) 
+                return "";
+            if(word2.length() > word1.length() && word1.startsWith(word2)) 
+                continue;
             
             for(int j=0; j<Math.min(word1.length(), word2.length()); j++) {
-                char prev = word1.charAt(j);
-                char next = word2.charAt(j);
-                if(prev != next) {
-                    GNode prevNode = graph.get(prev);
-                    GNode nextNode = graph.get(next);
-                    prevNode.outNodes.add(next);
-                    nextNode.inDegree++;
+                char c1 = word1.charAt(j);
+                char c2 = word2.charAt(j);
+                if(c1 != c2) {
+                    graph.get(c2).inDegree++;
+                    graph.get(c1).outedges.add(c2);
                     break;
                 }
             }
         }
         
-        char[] result = new char[graph.size()];
-        int size = 0;
-        
-        for(Map.Entry<Character, GNode> entry : graph.entrySet()) {
+        char result[] = new char[graph.size()];
+        int index = 0;
+        for(Map.Entry<Character,GNode> entry : graph.entrySet()) {
             if(entry.getValue().inDegree == 0) {
-                result[size++] = entry.getKey();
+                result[index++] = entry.getKey();
             }
         }
         
         int i=0;
-        while(i<size) {
-            char temp = result[i++];
-            GNode currNode = graph.get(temp);
-            for(Character outNode : currNode.outNodes) {
-                GNode nextNode = graph.get(outNode);
-                nextNode.inDegree--;
-                if(nextNode.inDegree == 0) {
-                    result[size++] = outNode;
+        while(i<index) {
+            char curr = result[i++];
+            GNode curNode = graph.get(curr);
+            for(Character child : curNode.outedges) {
+                GNode childNode = graph.get(child);
+                childNode.inDegree --;
+                if(childNode.inDegree == 0) {
+                    result[index++] = child;
                 }
-            } 
+            }
         }
         
-        if(size == graph.size()) {
+        if(index == graph.size()) {
             return new String(result);
         } else {
             return "";
         }
+        
     }
 }
