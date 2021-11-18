f969873e4e07988a082e1fa7245a36610411cf80
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2021-11-18 20:27:09 +0000

    Sync LeetCode submission - Count and Say (java)

--- LeetCodeSolutions/problems/count_and_say/solution.java
+++ LeetCodeSolutions/problems/count_and_say/solution.java
@@ -1,23 +1,24 @@
 class Solution {
     public String countAndSay(int n) {
-        if(n==1) {
-            return "1";
+        if(n==1) return "1";
+        
+        String prev = countAndSay(n-1);
+        StringBuilder result = new StringBuilder();
+        
+        int m = prev.length();
+        char c = prev.charAt(0);
+        int i=1, count = 1;
+        while(i<m) {
+            if(prev.charAt(i) == c) count++;
+            else {
+                result.append(count).append(c);
+                c = prev.charAt(i);
+                count = 1;
+            }
+            i++;
         }
-        String prevOne = countAndSay(n-1);
-        Character prev = prevOne.charAt(0);
-        int count=1;
-        String result = "";
-        int i=1;
-        for(i=1; i<prevOne.length(); i++) {
-              if(prev == prevOne.charAt(i)) {
-                  count++;
-              } else {
-                  result+= String.valueOf(count) + prevOne.charAt(i-1);
-                  count=1;
-                  prev = prevOne.charAt(i);
-              }
-        }
-        result+= String.valueOf(count) + prevOne.charAt(i-1);
-        return result;
+        result.append(count).append(c);
+        return result.toString();
+        
     }
 }
