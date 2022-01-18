940bdd50d04d80c64d63a5593b6b3fe3796783d5
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2022-01-18 15:21:01 +0000

    Sync LeetCode submission - Merge k Sorted Lists (java)

--- LeetCodeSolutions/problems/merge_k_sorted_lists/solution.java
+++ LeetCodeSolutions/problems/merge_k_sorted_lists/solution.java
@@ -11,48 +11,39 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         int n = lists.length;
-        if(n<1) return null;
+        if(n <= 0) return null;
         if(n==1) return lists[0];
         
-        ListNode[] newlists = new ListNode[(n+1)/2];
-        int count = 0;
+        ListNode[] newLists = new ListNode[(n+1)/2];
+        int count=0;
         
-        for(int i=0; i<n-1; i=i+2) {
+        for(int i=0; i<n-1; i+=2) {
             ListNode first = lists[i];
             ListNode second = lists[i+1];
             
             ListNode temp = new ListNode(0);
-            ListNode t = temp;
+            ListNode tHead = temp;
             
             while(first!=null && second!=null) {
                 if(first.val < second.val) {
                     temp.next = first;
-                    temp = temp.next;
                     first = first.next;
                 } else {
                     temp.next = second;
-                    temp = temp.next;
                     second = second.next;
                 }
+                temp = temp.next;
             }
             if(first == null) {
-                while(second != null) {
-                    temp.next = second;
-                    temp = temp.next;
-                    second = second.next;
-                }
+                temp.next = second;
             } else {
-                while(first!=null) {
-                    temp.next = first;
-                    temp = temp.next;
-                    first = first.next;
-                }
+                temp.next = first;
             }
-            newlists[count++] = t.next;
+            newLists[count++] = tHead.next;
         }
         if(n%2 == 1) {
-            newlists[count] = lists[n-1];
+            newLists[count] = lists[n-1];
         }
-        return mergeKLists(newlists);
+        return mergeKLists(newLists);
     }
 }
