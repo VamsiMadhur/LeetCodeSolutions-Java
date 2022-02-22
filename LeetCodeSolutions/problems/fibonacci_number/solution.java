d02eaa7899a06eb4f9d95a01376f6eabe13ef553
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2022-02-22 20:39:09 +0000

    Sync LeetCode submission - Fibonacci Number (java)

--- LeetCodeSolutions/problems/fibonacci_number/solution.java
+++ LeetCodeSolutions/problems/fibonacci_number/solution.java
@@ -1,12 +1,12 @@
 class Solution {
     public int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        int a = 0, b= 1;
+        if(n < 2) return n;
+        int a = 0;
+        int b = 1;
         for(int i=2; i<=n; i++) {
-            int temp = b;
-            b = a + b;
-            a = temp;
+            int temp = a+b;
+            a = b;
+            b = temp;
         }
         return b;
     }
