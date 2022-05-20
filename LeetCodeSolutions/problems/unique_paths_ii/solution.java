aa05c6fe39f5e1a45783849c4113384bb880bbff
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2022-05-20 02:22:59 +0000

    Sync LeetCode submission - Unique Paths II (java)

--- LeetCodeSolutions/problems/unique_paths_ii/solution.java
+++ LeetCodeSolutions/problems/unique_paths_ii/solution.java
@@ -1,24 +1,22 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int rows = obstacleGrid.length;
-        int columns = obstacleGrid[0].length;
-        
-        if(obstacleGrid[0][0] == 1 || obstacleGrid[rows-1][columns-1] == 1) {
-            return 0;
-        }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if(m==1 && n==1 && obstacleGrid[0][0]==0) return 1;
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1) return 0;
         
         obstacleGrid[0][0] = 1;
         
-        for(int i=1; i<rows; i++) {
+        for(int i=1; i<m; i++) {
             obstacleGrid[i][0] = (obstacleGrid[i][0]==0 && obstacleGrid[i-1][0]==1) ? 1 : 0;
         }
         
-        for(int i=1; i<columns; i++) {
-            obstacleGrid[0][i] = (obstacleGrid[0][i]==0 && obstacleGrid[0][i-1]==1) ? 1 : 0;
+        for(int j=1; j<n; j++) {
+            obstacleGrid[0][j] = (obstacleGrid[0][j]==0 && obstacleGrid[0][j-1]==1) ? 1 : 0;
         }
         
-        for(int i=1; i<rows; i++) {
-            for(int j=1; j<columns; j++) {
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
                 if(obstacleGrid[i][j] == 0) {
                     obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
                 } else {
@@ -26,7 +24,6 @@ class Solution {
                 }
             }
         }
-        
-        return obstacleGrid[rows-1][columns-1];
+        return obstacleGrid[m-1][n-1];
     }
 }
