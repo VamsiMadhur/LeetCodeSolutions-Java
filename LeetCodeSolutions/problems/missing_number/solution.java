0fb80e61e14b845a1e68b807ebce228022a131a4
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2022-05-28 02:46:53 +0000

    Sync LeetCode submission - Missing Number (java)

--- LeetCodeSolutions/problems/missing_number/solution.java
+++ LeetCodeSolutions/problems/missing_number/solution.java
@@ -1,20 +1,8 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        if(nums.length == 1) {
-            return 1 - nums[0];
-        }
         int n = nums.length;
-        for(int i=0; i<n; i++) {
-            if((nums[i]%(n+1))!=n) {
-                int key = nums[i]%(n+1);
-                nums[key] = nums[key]+n+1;
-            }
-        }
-        
-        int missing = n;
-        for(int i=0; i<n; i++) {
-            if(nums[i] <= n) missing = i;
-        }
-        return missing;
+        int totalSum = (n*(n+1))/2;
+        for(int num : nums) totalSum -= num;
+        return totalSum;
     }
 }
