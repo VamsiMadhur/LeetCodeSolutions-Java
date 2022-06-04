11a53830ad4ccedd06700784a0542cc27e8536c6
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2022-06-04 03:04:54 +0000

    Sync LeetCode submission - N-Queens (java)

--- LeetCodeSolutions/problems/n-queens/solution.java
+++ LeetCodeSolutions/problems/n-queens/solution.java
@@ -1,66 +1,57 @@
 class Solution {
     char[][] board;
-    List<List<String>> result;
-    int size = 0;
+    List<List<String>> result = new ArrayList<>();
+    int size=0;
     public List<List<String>> solveNQueens(int n) {
-        result = new ArrayList<>();
-        size = n;
+        size=n;
         
         if(n==1) {
             result.add(new ArrayList<>(){{add("Q");}});
             return result;
-        } 
-        if(n==2 || n==3) {
+        } else if(n==2 || n==3) {
             return result;
         }
-        
         this.board = new char[n][n];
-        
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
+
+        for(int i=0; i<n; i++)
+            for(int j=0; j<n; j++)
                 board[i][j] = '.';
-            }
-        }
-        
-        for(int i=0; i<size; i++) {
-            for(int j=0; j<size; j++) {
-                helper(i, j, n);        
-            }
-        }
+
+        for(int i=0; i<n; i++)
+                helper(0, i, n);
         
         return result;
     }
     
-   public void helper(int i, int j, int n) {
-       
+    public void helper(int i, int j, int n) {
         board[i][j] = 'Q';
-        n = n-1;
-        if(n==0) toString(board);
+        n--;
         
-        for(int r=i+1;r<size; r++) {
-        	for(int c=0; c<size; c++) {
-        		if(isValid(r, c)) {
-        			helper(r, c, n);
-        		}
-        	}
+        if(n==0)  toString(board);
+        
+        for(int r=i+1; r<size; r++) {
+            for(int c=0; c<size; c++) {
+                if(isValid(r, c))
+                    helper(r, c, n);
+            }
         }
         board[i][j] = '.';
     }
     
     public boolean isValid(int r, int c) {
         for(int i=0; i<r; i++) {
-            if(board[r][i] == 'Q') return false;
+            if(board[i][c] == 'Q') return false;
         }
         
         for(int i=0; i<size; i++) {
             if(i == r) continue;
-            if(board[i][c] == 'Q') return false;
+            if(board[r][i] == 'Q') return false;
         }
         
         int i=r-1, j=c-1;
         while(i>=0 && j>=0) {
             if(board[i][j] == 'Q') return false;
-            i--; 
+            i--;
             j--;
         }
         
@@ -78,6 +69,7 @@ class Solution {
 
         for(int i=0; i<size; i++) 
             temp.add(new String(board[i]));
+        
         result.add(temp);
     }
 }
