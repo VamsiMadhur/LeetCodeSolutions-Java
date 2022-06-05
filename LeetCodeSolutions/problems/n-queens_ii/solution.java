d6b6402daad92b82dbce57f113085de4dbe63b3d
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2022-06-05 00:32:34 +0000

    Sync LeetCode submission - N-Queens II (java)

--- LeetCodeSolutions/problems/n-queens_ii/solution.java
+++ LeetCodeSolutions/problems/n-queens_ii/solution.java
@@ -1,64 +1,56 @@
 class Solution {
-    
     char[][] board;
     int result = 0;
-    int size = 0;
+    int size=0;
+   
     public int totalNQueens(int n) {
-        size = n;
-        
-        if(n==1) {
-            return 1;
-        } 
-        if(n==2 || n==3) {
-            return 0;
-        }
+        size=n;
+        if(n==1) return 1;
+        if(n==2 || n==3) return 0;
         
         this.board = new char[n][n];
-        
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
+
+        for(int i=0; i<n; i++)
+            for(int j=0; j<n; j++)
                 board[i][j] = '.';
-            }
-        }
-        
-        for(int i=0; i<size; i++) {
-            for(int j=0; j<size; j++) {
-                helper(i, j, n);        
-            }
-        }
+
+        for(int i=0; i<n; i++)
+                helper(0, i, n);
         
         return result;
     }
     
-   public void helper(int i, int j, int n) {
+    public void helper(int i, int j, int n) {
         board[i][j] = 'Q';
-        n = n-1;
-        if(n==0) result++;
+        n--;
+        
+        if(n==0) {
+            result++;
+        }
         
         for(int r=i+1; r<size; r++) {
-        	for(int c=0; c<size; c++) {
-        		if(isValid(r, c)) {
-        			helper(r, c, n);
-        		}
-        	}
+            for(int c=0; c<size; c++) {
+                if(isValid(r, c))
+                    helper(r, c, n);
+            }
         }
         board[i][j] = '.';
     }
     
     public boolean isValid(int r, int c) {
         for(int i=0; i<r; i++) {
-            if(board[r][i] == 'Q') return false;
+            if(board[i][c] == 'Q') return false;
         }
         
         for(int i=0; i<size; i++) {
             if(i == r) continue;
-            if(board[i][c] == 'Q') return false;
+            if(board[r][i] == 'Q') return false;
         }
         
         int i=r-1, j=c-1;
         while(i>=0 && j>=0) {
             if(board[i][j] == 'Q') return false;
-            i--; 
+            i--;
             j--;
         }
         
@@ -69,5 +61,5 @@ class Solution {
             j++;
         }
         return true;
-    }
+    }    
 }
