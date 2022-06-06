a8fc111e3ffe4de9a1f56e15e25a3daa3ac441bf
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2022-06-06 02:14:51 +0000

    Sync LeetCode submission - Intersection of Two Linked Lists (java)

--- LeetCodeSolutions/problems/intersection_of_two_linked_lists/solution.java
+++ LeetCodeSolutions/problems/intersection_of_two_linked_lists/solution.java
@@ -11,30 +11,21 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        if(headA == null || headB == null) {
-            return null;
+        ListNode ta = headA, tb=headB;
+        boolean flagA = false, flagB = false;
+        while(ta != tb) {
+            ta = ta.next;
+            tb = tb.next;
+            if(!flagA && ta == null) {
+                ta = headB;
+                flagA = true;
+            }
+            if(!flagB && tb == null) {
+                tb = headA;
+                flagB = true;
+            }
+            if((flagA && ta == null) || (flagB && tb == null)) return null;
         }
-        ListNode p1 = headA;
-        ListNode p2 = headB;
-        boolean a = false;
-        boolean b = false;
-        while(!a || !b || (p1 !=null && p2!=null)) {
-            if(p1 == p2) {
-                return p1;
-            }
-            if(p1 == null) {
-                a = true;
-                p1 = headB;
-            } else {
-                p1 = p1.next;    
-            }
-            if(p2 == null) {
-                b = true;
-                p2 = headA;
-            } else {
-                p2 = p2.next;    
-            }
-        }
-        return null;
+        return ta;
     }
 }
