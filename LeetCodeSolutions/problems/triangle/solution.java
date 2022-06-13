4d8d0fe6a5c529d78994d7bc763f00b9aafd3be9
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2022-06-13 00:24:11 +0000

    Sync LeetCode submission - Triangle (java)

--- LeetCodeSolutions/problems/triangle/solution.java
+++ LeetCodeSolutions/problems/triangle/solution.java
@@ -2,18 +2,16 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
         int[] minSum = new int[n];
-        List<Integer> lastRow = triangle.get(n-1);
-        for(int i=0; i<n; i++) {
+        List<Integer> lastRow =  triangle.get(n-1);
+        for(int i=0; i<n; i++) 
             minSum[i] = lastRow.get(i);
-        }
         
         for(int i=n-2; i>=0; i--) {
-            List<Integer> currRow = triangle.get(i);
-            for(int j=0; j<=i; j++) {
-                int temp = Math.min(minSum[j], minSum[j+1]);
-                minSum[j] = currRow.get(j) + temp;
-            }
+            List<Integer> currRow =  triangle.get(i);
+            for(int j=0; j<=i; j++) 
+                minSum[j] = Math.min(minSum[j], minSum[j+1])+currRow.get(j);
         }
         return minSum[0];
+        
     }
 }
