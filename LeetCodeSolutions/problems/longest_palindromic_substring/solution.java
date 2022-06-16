0493b9f1dc0989c1e8d55bdaf50f74038674e8c4
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2022-06-16 00:50:38 +0000

    Sync LeetCode submission - Longest Palindromic Substring (java)

--- LeetCodeSolutions/problems/longest_palindromic_substring/solution.java
+++ LeetCodeSolutions/problems/longest_palindromic_substring/solution.java
@@ -1,33 +1,29 @@
 class Solution {
+    int x=0, y=0, n=0;
     public String longestPalindrome(String s) {
-        int n = s.length();
-        boolean[][] dp = new boolean [n][n];
-        char[] sa = s.toCharArray();
-        
-        int maxLength = 0;
-        int x=0; int y=0;
-        for(int i=0; i<n; i++) {
-            dp[i][i] = true;
-            if(i!= n-1 && sa[i] == sa[i+1]) {
-                maxLength = 2;
-                dp[i][i+1] = true;
-                x = i;
-                y = i+1;
-            }
-        }
-        
-        for(int i=n-3; i>=0; i--) {
-            for(int j=i+2; j<n; j++) {
-                if(sa[i] == sa[j] && dp[i+1][j-1]) {
-                    dp[i][j] = true;
-                    if(j-i+1 > maxLength) {
-                        x=i;
-                        y=j;
-                        maxLength = j-i+1;
-                    }
-                } 
-            }
-        }
+        n = s.length();
+        if(n<2) return s;
+        helper(s.toCharArray(), 0);
         return s.substring(x, y+1);
+    }
+    
+    public void helper(char[] sa, int index) {
+        int a=index, b=index;
+        if(n==index-1 || (n-index) < (y-x+1)/2)  return;
+        
+        while(b<n-1 && sa[b] == sa[b+1]) {
+            b++;
+        }
+        index=b;
+        
+        while(a>0 && b<n-1 && sa[a-1] == sa[b+1]) {
+            a--; b++;
+        }
+        
+        if(b-a > y-x) {
+            x=a;
+            y=b;
+        }
+        helper(sa, index+1);
     }
 }
