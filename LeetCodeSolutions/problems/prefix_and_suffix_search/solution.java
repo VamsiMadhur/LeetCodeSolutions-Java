6da9d17df9f3a63c509f1acacb848dd04bc77c3a
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2022-06-20 15:07:39 +0000

    Sync LeetCode submission - Prefix and Suffix Search (java)

--- LeetCodeSolutions/problems/prefix_and_suffix_search/solution.java
+++ LeetCodeSolutions/problems/prefix_and_suffix_search/solution.java
@@ -1,39 +1,40 @@
+class TrieNode {
+    TrieNode[] child = new TrieNode[26];
+    List<Integer> indexList = new ArrayList<>();
+    
+}
+
 class WordFilter {
-    TrieNode trie;
+    TrieNode root;
+    String[] words;
+    
     public WordFilter(String[] words) {
-        trie = new TrieNode();
-        for (int weight = 0; weight < words.length; ++weight) {
-            String word = words[weight] + "{";
-            for (int i = 0; i < word.length(); ++i) {
-                TrieNode cur = trie;
-                cur.weight = weight;
-                for (int j = i; j < 2 * word.length() - 1; ++j) {
-                    int k = word.charAt(j % word.length()) - 'a';
-                    if (cur.children[k] == null)
-                        cur.children[k] = new TrieNode();
-                    cur = cur.children[k];
-                    cur.weight = weight;
-                }
+        root = new TrieNode();
+        this.words = words;
+        for(int i=0; i<words.length; i++) {
+            TrieNode curr = root;
+            for(char c : words[i].toCharArray()) {
+                if(curr.child[c-'a'] == null) curr.child[c-'a'] = new TrieNode();
+                curr.child[c-'a'].indexList.add(i);
+                curr = curr.child[c-'a'];
             }
         }
     }
+    
     public int f(String prefix, String suffix) {
-        TrieNode cur = trie;
-        for (char letter: (suffix + '{' + prefix).toCharArray()) {
-            if (cur.children[letter - 'a'] == null) return -1;
-            cur = cur.children[letter - 'a'];
+        TrieNode curr = root;
+        for(char c : prefix.toCharArray()) {
+            curr = curr.child[c-'a'];
+            if(curr == null) return -1;
         }
-        return cur.weight;
-    }
-}
-
-class TrieNode {
-    TrieNode[] children;
-    int weight;
-    public TrieNode() {
-        children = new TrieNode[27];
-        weight = 0;
-    }
+        
+        List<Integer> list = curr.indexList;
+        for(int i=list.size()-1; i>=0; i--) {
+            if(words[list.get(i)].endsWith(suffix)) 
+                return list.get(i);
+        }
+        return -1;
+     }
 }
 
 /**
