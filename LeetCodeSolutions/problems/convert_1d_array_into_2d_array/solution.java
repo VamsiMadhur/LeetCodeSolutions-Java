af1f109db59a068909dd8ed8c53ba611649f4305
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2022-06-29 21:20:14 +0000

    Sync LeetCode submission - Convert 1D Array Into 2D Array (java)

---
+++ LeetCodeSolutions/problems/convert_1d_array_into_2d_array/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int p = original.length;
+        if((m*n) != p) return new int[0][0];
+        
+        int[][] result = new int[m][n];
+        int k=0;
+        for(int i=0; i<m; i++)
+            for(int j=0; j<n; j++)
+                result[i][j] = original[k++];
+        
+        return result;
+    }
+}
