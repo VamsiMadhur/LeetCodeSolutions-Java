eaf5f7a721a966abd02376d16c4331f0b57128f2
VamsiMadhur <91028690+VamsiMadhur@users.noreply.github.com>
2022-06-29 23:13:55 +0000

    Sync LeetCode submission - Course Schedule III (java)

--- LeetCodeSolutions/problems/course_schedule_iii/solution.java
+++ LeetCodeSolutions/problems/course_schedule_iii/solution.java
@@ -1,15 +1,15 @@
 public class Solution {
     public int scheduleCourse(int[][] courses) {
-        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
-        PriorityQueue < Integer > queue = new PriorityQueue < > ((a, b) -> b - a);
-        int time = 0;
-        for (int[] c: courses) {
-            if (time + c[0] <= c[1]) {
-                queue.offer(c[0]);
-                time += c[0];
-            } else if (!queue.isEmpty() && queue.peek() > c[0]) {
-                time += c[0] - queue.poll();
-                queue.offer(c[0]);
+        Arrays.sort(courses, (a,b) -> (a[1]-b[1]));
+        PriorityQueue<int[]> queue = new PriorityQueue<>((a,b)->(b[0]-a[0]));
+        int time=0;
+        for(int[] course : courses) {
+            if(time + course[0] <= course[1]) {
+                queue.offer(course);
+                time += course[0];
+            } else if(!queue.isEmpty() && queue.peek()[0] > course[0]) {
+                time += course[0] - queue.poll()[0];
+                queue.offer(course);
             }
         }
         return queue.size();
